d63189eddc6682c2ab1a30aa2ae4d06512fbf58f
HOME <commontoday@naver.com>
2018-04-16 05:40:55 +0900

    permission updating

--- Flashlight_Sensor/app/src/main/java/com/inhatc/jinuk/flashlight_sensor/MainActivity.java
+++ Flashlight_Sensor/app/src/main/java/com/inhatc/jinuk/flashlight_sensor/MainActivity.java
@@ -1,32 +1,28 @@
 package com.inhatc.jinuk.flashlight_sensor;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// 레퍼런스 : https://developer.android.com/training/permissions/requesting.html#perm-request
+// 도움 : http://kerneler.tistory.com/33
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
-
-    private boolean checkMERAPermission(){
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
-        return result == PackageManager.PERMISSION_GRANTED;
-    }
 
     SensorManager objSMG;
     Sensor sensor_Illuminance;
     ImageView objIMG;
-    android.hardware.Camera objCAM;
-    android.hardware.Camera.Parameters objCamPara;
+    Camera objCAM;
+    Camera.Parameters objCamPara;
 
     TextView objTV_Lux;
 
@@ -35,12 +31,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)){
+                // 권한 요청
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+            }
+        }else{
+            // 카메라 실행
+            //Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        }
+
+        //objCAM = Camera.open();
+        objCAM = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
         objSMG = (SensorManager)getSystemService(SENSOR_SERVICE);
-        sensor_Illuminance = objSMG.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sensor_Illuminance = objSMG.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        objCAM = android.hardware.Camera.open();
         objIMG = (ImageView) findViewById(R.id.imageView);
-
         objTV_Lux = (TextView) findViewById(R.id.txtLux);
     }
 
@@ -69,12 +75,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             objTV_Lux.setText(("Lux: " + iLux));
             if(iLux <= 80){
                 objIMG.setImageResource(R.drawable.lighton);
-                objCamPara.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
+                objCamPara = objCAM.getParameters();
+                objCamPara.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                 objCAM.setParameters(objCamPara);
                 objCAM.startPreview();
             }else {
                 objIMG.setImageResource(R.drawable.lightoff);
-                objCamPara.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
+                objCamPara = objCAM.getParameters();
+                objCamPara.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
                 objCAM.setParameters(objCamPara);
                 objCAM.stopPreview();
             }
