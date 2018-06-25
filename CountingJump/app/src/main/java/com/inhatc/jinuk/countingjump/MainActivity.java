ed6e3b892470a1af10c6afc3b9c38f5aee34da7d
HOME <commontoday@naver.com>
2018-06-25 23:50:18 +0900

    실행되게끔 수정, setText 시 값이 정수혹은 실수라면 String.valueOf()로 변경을 해줘야 에러가 사라진다.

--- CountingJump/app/src/main/java/com/inhatc/jinuk/countingjump/MainActivity.java
+++ CountingJump/app/src/main/java/com/inhatc/jinuk/countingjump/MainActivity.java
@@ -6,24 +6,24 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     SensorManager objSMG;
     Sensor sensor_Accelerometer;
-    TextView objTV_Y_Accelerometer;
-    int num;
+    TextView objTV_Accelerometer;
+    int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num = 0;
-
+        objSMG = (SensorManager)getSystemService(SENSOR_SERVICE);
         sensor_Accelerometer = objSMG.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        objTV_Y_Accelerometer = (TextView) findViewById(R.id.CountingNumber);
+        objTV_Accelerometer = findViewById(R.id.CountingNumber);
         }
     @Override
     public void onResume() {
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            if(sensorEvent.values[0] > 5){
-                objTV_Y_Accelerometer.setText(num ++);
+            if(sensorEvent.values[0] > 7){
+                objTV_Accelerometer.setText(String.valueOf(num++));
             }
         }
     }
